64fae5370b87c168cddca3b559ae023ff5497227
tsantos <tales.augusto.santos@gmail.com>
2018-04-27 17:12:49 -0300

    Added autoconfigure option in services

---
+++ tests/Fixture/Post.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\_NAMESPACE_;
+
+/**
+ * Class Post
+ *
+ * @author Tales Santos <tales.augusto.santos@gmail.com>
+ */
+class Post
+{
+    /** @var integer */
+    private $id;
+
+    /** @var string */
+    private $title;
+
+    /** @var string */
+    private $summary;
+
+    /**
+     * Post constructor.
+     * @param int $id
+     * @param string $title
+     * @param string $summary
+     */
+    public function __construct(int $id, string $title, string $summary)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->summary = $summary;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSummary(): string
+    {
+        return $this->summary;
+    }
+
+    /**
+     * @param string $summary
+     */
+    public function setSummary(string $summary): void
+    {
+        $this->summary = $summary;
+    }
+}
