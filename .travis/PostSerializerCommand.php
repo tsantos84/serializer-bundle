a349f4c1651087e5ad3e38363486d98ec331ffec
tsantos <tales.augusto.santos@gmail.com>
2018-07-02 10:46:58 -0300

    Configuring bundle into symfony demo application

---
+++ .travis/PostSerializerCommand.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of the TSantos Serializer Bundle package.
+ *
+ * (c) Tales Santos <tales.augusto.santos@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Command;
+
+use App\Repository\PostRepository;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use TSantos\Serializer\SerializerAwareInterface;
+use TSantos\Serializer\Traits\SerializerAwareTrait;
+
+/**
+ * Class SerializerCommand
+ *
+ * @author Tales Santos <tales.augusto.santos@gmail.com>
+ */
+class PostSerializerCommand extends Command implements SerializerAwareInterface
+{
+    use SerializerAwareTrait;
+
+    /**
+     * @var PostRepository
+     */
+    private $posts;
+
+    /**
+     * PostSerializerCommand constructor.
+     * @param PostRepository $posts
+     */
+    public function __construct(PostRepository $posts)
+    {
+        parent::__construct();
+        $this->posts = $posts;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Serialize posts for test purpose')
+            ->setName('serialize:posts');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln($this->serializer->serialize($this->posts->find(1)));
+    }
+}
