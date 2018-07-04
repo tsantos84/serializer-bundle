b496efd0c84f8e129bfca13d2d631d0f6d81e62c
tsantos <tales.augusto.santos@gmail.com>
2018-07-04 01:09:45 -0300

    Add circular reference handler

---
+++ Serializer/CircularReferenceHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the TSantos Serializer Bundle package.
+ *
+ * (c) Tales Santos <tales.augusto.santos@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TSantos\SerializerBundle\Serializer;
+
+use TSantos\Serializer\Exception\CircularReferenceException;
+
+/**
+ * Class CircularReferenceHandler.
+ *
+ * @author Tales Santos <tales.augusto.santos@gmail.com>
+ */
+class CircularReferenceHandler
+{
+    public function __invoke($data, CircularReferenceException $circularReferenceException)
+    {
+        $name = method_exists($data, '__toString')
+            ? (string) $data
+            : spl_object_hash($data);
+
+        return $name;
+    }
+}
