d1faccacaf0bc678375b3eea668b3aa777b24f72
tsantos <tales.augusto.santos@gmail.com>
2018-08-04 12:51:05 -0300

    List all auto mapped classes and its mapping

--- DataCollector/SerializerCollector.php
+++ DataCollector/SerializerCollector.php
@@ -15,7 +15,9 @@ use Metadata\Driver\AdvancedDriverInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use TSantos\Serializer\Metadata\ClassMetadata;
 use TSantos\Serializer\Metadata\PropertyMetadata;
+use TSantos\SerializerBundle\Service\ClassLocator;
 
 /**
  * Class SerializerCollector
@@ -35,53 +37,48 @@ class SerializerCollector extends DataCollector
     private $advancedDrivers;
 
     /**
+     * @var ClassLocator
+     */
+    private $classLocator;
+
+    /**
      * SerializerCollector constructor.
      * @param AdvancedMetadataFactoryInterface $metadataFactory
-     * @param AdvancedDriverInterface[] $advancedDrivers
+     * @param array $advancedDrivers
+     * @param ClassLocator $classLocator
      */
-    public function __construct(AdvancedMetadataFactoryInterface $metadataFactory, array $advancedDrivers)
-    {
+    public function __construct(
+        AdvancedMetadataFactoryInterface $metadataFactory,
+        array $advancedDrivers,
+        ClassLocator $classLocator
+    ) {
         $this->metadataFactory = $metadataFactory;
         $this->advancedDrivers = $advancedDrivers;
+        $this->classLocator = $classLocator;
     }
 
     public function collect(Request $request, Response $response, \Exception $exception = null)
     {
         $this->data = [
-            'mapped_classes' => []
+            'mapped_classes' => [],
+            'auto_mapped_classes' => []
         ];
 
-        $classes = array_map(function (AdvancedDriverInterface $driver) {
-            return $driver->getAllClassNames();
-        }, $this->advancedDrivers);
+        $mappedClasses = $this->doGetMappedClasses();
+        $autoMappedClasses = $this->doGetAutoMappedClasses(array_keys($mappedClasses));
 
-        $iterator = new \RecursiveIteratorIterator(new \RecursiveArrayIterator($classes));
-
-        foreach ($iterator as $className) {
-
-            $metadata = $this->metadataFactory->getMetadataForClass($className);
-
-            $mapping = array_map(function (PropertyMetadata $propertyMetadata): array {
-                return [
-                    'name' => $propertyMetadata->name,
-                    'exposeAs' => $propertyMetadata->exposeAs,
-                    'type' => $propertyMetadata->type,
-                    'options' => $propertyMetadata->options,
-                ];
-            }, $metadata->propertyMetadata);
-
-            $this->data['mapped_classes'][] = [
-                'name' => $className,
-                'filename' => $metadata->reflection->getFileName(),
-                'line' => $metadata->reflection->getStartLine(),
-                'mapping' => $this->cloneVar($mapping)
-            ];
-        }
+        $this->data['mapped_classes'] = $this->createMappingInfo($mappedClasses);
+        $this->data['auto_mapped_classes'] = $this->createMappingInfo($autoMappedClasses);
     }
 
     public function getMappedClasses(): array
     {
         return $this->data['mapped_classes'];
+    }
+
+    public function getAutoMappedClasses(): array
+    {
+        return $this->data['auto_mapped_classes'];
     }
 
     public function getName()
@@ -92,5 +89,70 @@ class SerializerCollector extends DataCollector
     public function reset()
     {
         $this->data = [];
+    }
+
+    /**
+     * @return ClassMetadata[]
+     */
+    private function doGetMappedClasses(): array
+    {
+        $mapped = [];
+
+        $classes = array_map(function (AdvancedDriverInterface $driver) {
+            return $driver->getAllClassNames();
+        }, $this->advancedDrivers);
+
+        $iterator = new \RecursiveIteratorIterator(new \RecursiveArrayIterator($classes));
+
+        foreach ($iterator as $className) {
+            $mapped[$className] = $this->metadataFactory->getMetadataForClass($className);
+        }
+
+        return $mapped;
+    }
+
+    private function doGetAutoMappedClasses(array $excluded): array
+    {
+        try {
+            $classes = $this->classLocator->findAllClasses();
+        } catch (\LogicException | \InvalidArgumentException $e) {
+            return [];
+        }
+
+        $classes = array_diff($classes, $excluded);
+
+        return array_map(function(string $class) {
+            return $this->metadataFactory->getMetadataForClass($class);
+        }, $classes);
+    }
+
+    /**
+     * @param ClassMetadata[] $metadataClasses
+     * @return array
+     */
+    private function createMappingInfo(array $classes): array
+    {
+        $info = [];
+        foreach ($classes as $metadata) {
+
+            $mapping = array_map(function (PropertyMetadata $propertyMetadata): array {
+                return [
+                    'name' => $propertyMetadata->name,
+                    'exposeAs' => $propertyMetadata->exposeAs,
+                    'type' => $propertyMetadata->type,
+                    'options' => $propertyMetadata->options,
+                ];
+            }, $metadata->propertyMetadata);
+
+            $info[] = [
+                'name' => $metadata->name,
+                'filename' => $metadata->reflection->getFileName(),
+                'path' => $metadata->reflection->getFilename(),
+                'line' => $metadata->reflection->getStartLine(),
+                'mapping' => $this->cloneVar($mapping)
+            ];
+        }
+
+        return $info;
     }
 }
---
+++ Service/ClassLocator.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This file is part of the TSantos Serializer Bundle package.
+ *
+ * (c) Tales Santos <tales.augusto.santos@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TSantos\SerializerBundle\Service;
+
+/**
+ * Class ClassLocator
+ *
+ * @author Tales Santos <tales.augusto.santos@gmail.com>
+ */
+class ClassLocator
+{
+    /**
+     * @var ClassNameReader
+     */
+    private $classNameReader;
+
+    /**
+     * @var array
+     */
+    private $directories;
+
+    /**
+     * @var array
+     */
+    private $excluded;
+
+    /**
+     * ClassLocator constructor.
+     * @param ClassNameReader $classNameReader
+     * @param array $directories
+     * @param array $excluded
+     */
+    public function __construct(ClassNameReader $classNameReader, array $directories, array $excluded = [])
+    {
+        $this->classNameReader = $classNameReader;
+        $this->directories = $directories;
+        $this->excluded = $excluded;
+    }
+
+    public function findAllClasses(): array
+    {
+        return $this->classNameReader->readDirectory($this->directories, $this->excluded);
+    }
+}
